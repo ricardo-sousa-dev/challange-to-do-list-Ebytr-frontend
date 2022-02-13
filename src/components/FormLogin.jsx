9e5c9462b4e75b8f4820141e802437a885f5c52a
ricardo-sousa <rwmsousa@gmail.com>
2022-02-13 18:10:15 -0300

    cria formulário de login inicial

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Tabs, Tab } from 'react-bootstrap';
 import '../css/components/formLogin.css';
 
 function FormLogin() {
   return (
     <div className="form-login">
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Insira seu email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Insira sua senha" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <Tabs defaultActiveKey="login" id="tab" className="mb-3">
+        <Tab eventKey="login" title="Login">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Insira seu email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type="password" placeholder="Insira sua senha" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Entrar
+            </Button>
+          </Form>
+        </Tab>
+        <Tab eventKey="register" title="Cadastro">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control type="text" placeholder="Insira seu email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Insira seu email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type="password" placeholder="Insira sua senha" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Cadastrar
+            </Button>
+          </Form>
+        </Tab>
+      </Tabs>
+
+
     </div>
   );
 }
