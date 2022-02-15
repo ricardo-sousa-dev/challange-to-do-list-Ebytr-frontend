3ea7f53c69a3ca87f1ffe2ddb12d053c5b8759d2
ricardo-sousa <rwmsousa@gmail.com>
2022-02-15 10:57:35 -0300

    corrige erros de conexção

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000'
-  baseURL: 'http://localhost:3000'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001'
 });
 
 export default api; 
