f1b1ff8820519862d55b2cdd5019e4af16ea1e27
ricardo-sousa <rwmsousa@gmail.com>
2022-02-16 15:32:58 -0300

    insere botão adicionar tarefa ao lado do input

--- src/components/InputNewTask.jsx
+++ src/components/InputNewTask.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
+import { Form } from 'react-bootstrap';
 import '../css/components/inputNewTask.css';
+import add_task from '../images/add_task.svg';
 
 function FormNewTask() {
 
   return (
-    <>
+    <div className="form-new-task">
       <Form.Control
         type="password"
         id="inputPassword5"
         aria-describedby="passwordHelpBlock"
         className="input-new-task"
       />
-    </>
+      <button type="button"><img src={add_task} alt="Add Task" /></button>
+    </div>
   );
 }
 
