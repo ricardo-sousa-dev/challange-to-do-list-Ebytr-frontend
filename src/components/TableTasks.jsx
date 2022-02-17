c43002bbb4742be84477b19734f2810d914a1ecc
ricardo-sousa <rwmsousa@gmail.com>
2022-02-17 11:11:59 -0300

    implementa o estilo do botão excluir uma tarefa

--- src/components/TableTasks.jsx
+++ src/components/TableTasks.jsx
@@ -11,7 +11,16 @@ function TableTasks() {
     api.get('/tasks',
       { headers: { 'authorization': userData.data.token } })
       .then(res => setTasks(res.data));
-  }, [isSaving]);
+  }, [ isSaving ]);
+
+  const updateTask = async (id) => {
+    setIsSaving(false);
+
+    api.update(`/tasks/${ id }`, id)
+      .then(res => res.data);
+
+    setIsSaving(true);
+  };
 
   const deleteTask = (idTask) => {
     setIsSaving(false);
@@ -32,7 +41,7 @@ function TableTasks() {
             <th className="task-table">Tarefa</th>
             <th className="date-table">Data</th>
             <th className="status-table">Status</th>
-            <th className="delete-table"></th>
+            <th className="buttons-table"></th>
           </tr>
         </thead>
         <tbody>
@@ -42,11 +51,20 @@ function TableTasks() {
               <td className="task-table">{task.task}</td>
               <td className="date-table">{task.createdAt}</td>
               <td className="status-table">{task.status}</td>
-              <td className="delete-table">
+              <td className="buttons-table">
+                <Button
+                  variant="outline-secondary"
+                  type="button"
+                  className="update-button"
+                  onClick={(event) => updateTask(event)}
+                >
+                  Editar
+                </Button>
                 <Button
                   variant="outline-secondary"
                   type="button"
                   value={task._id}
+                  className="delete-button"
                   onClick={(event) => deleteTask(event.target.value)}
                 >
                   Excluir
